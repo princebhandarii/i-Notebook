2db9de68c0069b0f85e94948ab269514d9e4cd2a
Prince22 <112156035+princebhandarii@users.noreply.github.com>
2024-04-06 15:28:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,35 @@
-const connectTOMongo = require('./db');
-const express = require('express')
-var cors=require('cors')
-var path = require('path')
+const connectTOMongo = require("./db");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 connectTOMongo();
-const app = express()
+const app = express();
 //const port = 5000
 const port = process.env.REACT_APP_PORT || 5000;
-app.use(express.json())
 
 
-app.use(cors())
-app.use(express.json())
+// Define CORS options
+const corsOptions = {
+  origin: ["https://notebook-back.vercel.app"], // Add any origins that you want to allow
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+};
+app.use(express.json());
+// Apply CORS middleware with custom options
+app.use(cors(corsOptions));
+app.use(express.json());
 
-//availabel Routes
-
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
-
+// Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 /// static file start
-
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+// });
 
 app.listen(port, () => {
-  console.log(`iNoteBook Backend listening on port http://localhost:${port}`)
-})
+  console.log(`iNoteBook Backend listening on port http://localhost:${port}`);
+});
